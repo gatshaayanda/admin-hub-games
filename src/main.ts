8d8d82cc57d9fd6fc19b29bcf64b9d111bfe6425
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-17 20:50:07 +0200

    feat: initialize mobile touch controls

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import './style.css';
+import './mobile-controls';
 import './firebase/firebase';
 import { BootScene } from './scenes/BootScene';
 import { PublisherIntroScene } from './scenes/PublisherIntroScene';
