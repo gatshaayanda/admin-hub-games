c610a60c47643eea95a5f2f8d536f43bf58a075d
gatshaayanda <93461785+gatshaayanda@users.noreply.github.com>
2026-09-18 19:42:49 +0200

    feat: register Admin Hub Games PWA

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import './style.css';
 import './mobile-controls';
 import './audio';
 import './firebase/firebase';
+import { registerPwa } from './pwa';
 import { BootScene } from './scenes/BootScene';
 import { PublisherIntroScene } from './scenes/PublisherIntroScene';
 import { NameEntryScene } from './scenes/NameEntryScene';
 import { HallIntroScene } from './scenes/HallIntroScene';
 import { GameShellScene } from './scenes/GameShellScene';
+
+registerPwa();
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
